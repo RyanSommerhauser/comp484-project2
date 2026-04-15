0a99443d1c23b76f634c11d9cea66ee638c96e97
RyanSommerhauser <ryansommerhauser@gmail.com>
2026-04-15 12:23:22 -0700

    Buttons and Variables working

--- script.js
+++ script.js
@@ -7,40 +7,76 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     $('.treat-button').click(clickedTreatButton);
     $('.play-button').click(clickedPlayButton);
     $('.exercise-button').click(clickedExerciseButton);
-  
+    $('.feed-button').click(clickedFeedButton);
 
   
     
   })
   
     // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
-    var pet_info = {name:"My Pet Name", weight:"??", happiness:"??"};
+    var pet_info = {
+      name: "Fluffy",
+      weight: 10,
+      happiness: 50,
+      hunger: 50
+    };
   
     function clickedTreatButton() {
-      // Increase pet happiness
-      // Increase pet weight
+      pet_info.happiness += 10;
+      pet_info.weight += 2;
       checkAndUpdatePetInfoInHtml();
     }
     
     function clickedPlayButton() {
-      // Increase pet happiness
-      // Decrease pet weight
+      pet_info.happiness += 10;
+      pet_info.weight -= 1;
+      pet_info.hunger -= 5;
       checkAndUpdatePetInfoInHtml();
     }
     
     function clickedExerciseButton() {
-      // Decrease pet happiness
-      // Decrease pet weight
+      pet_info.happiness -= 5;
+      pet_info.weight -= 3;
+      pet_info.hunger -= 5;
+      checkAndUpdatePetInfoInHtml();
+    }
+    function clickedFeedButton() {
+      pet_info.hunger += 25;
+      pet_info.weight += 2;
       checkAndUpdatePetInfoInHtml();
     }
   
     function checkAndUpdatePetInfoInHtml() {
-      checkWeightAndHappinessBeforeUpdating();  
+      checkBoundsBeforeUpdating();  
       updatePetInfoInHtml();
     }
+
+    setInterval(function() {
+      pet_info.hunger -= 1;
+      checkAndUpdatePetInfoInHtml();
+    }, 1000); // every 3 seconds
+
+    setInterval(function() {
+      pet_info.happiness -= 1;
+      checkAndUpdatePetInfoInHtml();
+    }, 3000); // every 3 seconds
     
-    function checkWeightAndHappinessBeforeUpdating() {
-      // Add conditional so if weight is lower than zero.
+    function checkBoundsBeforeUpdating() {
+      if (pet_info.weight < 0) {
+        pet_info.weight = 0;
+      }
+      if (pet_info.happiness < 0) {
+        pet_info.happiness = 0;
+      }
+      if (pet_info.happiness > 100) {
+        pet_info.happiness = 100;
+      }
+      if (pet_info.hunger < 0) {
+        pet_info.hunger = 0;
+      }
+      if (pet_info.hunger > 100) {
+        pet_info.hunger = 100;
+      }
     }
     
     // Updates your HTML with the current values in your pet_info object
@@ -48,5 +84,6 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
       $('.name').text(pet_info['name']);
       $('.weight').text(pet_info['weight']);
       $('.happiness').text(pet_info['happiness']);
+      $('.hunger').text(pet_info.hunger);
     }
   
